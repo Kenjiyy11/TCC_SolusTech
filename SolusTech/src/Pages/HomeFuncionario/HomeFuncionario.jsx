de477eb217a64ac056e5945b25642ce486384f92
unknown <yoneyabruno@gmail.com>
2026-06-16 19:38:57 -0300

    Criação do site

---
+++ SolusTech/src/Pages/HomeFuncionario/HomeFuncionario.jsx
@@ -0,0 +1,18 @@
+import '../MenuFuncionario/MenuFuncionario'
+
+const HomeFuncionario = ()  => {
+
+    return (
+        <div> 
+            
+            <MenuFuncionario />
+        
+        </div>
+            
+            
+            )
+            
+            
+}  
+
+export default HomeFuncionario;
