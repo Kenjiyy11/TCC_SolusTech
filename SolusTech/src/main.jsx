a7876eb318c187d9eabc054e061589eb081db111
Bruno Kenji Araujo Yoneya <rm98787@estudante.fieb.edu.br>
2026-06-26 11:02:08 -0300

    Placas

--- SolusTech/src/main.jsx
+++ SolusTech/src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import "bootstrap/dist/js/bootstrap.bundle.min"
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
