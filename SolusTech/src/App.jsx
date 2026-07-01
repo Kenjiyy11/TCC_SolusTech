7dcf47f7601c943219fd7fecc65196e095eb96cf
Breno Henrique Simiao <rm98155@estudante.fieb.edu.br>
2026-07-01 07:37:48 -0300

    local atualizado

--- SolusTech/src/App.jsx
+++ SolusTech/src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Solustech from './Pages/Home/Solustech.jsx'
-import Login from './Pages/Home/Logins/Login.jsx' 
-import Loja from './Pages/Home/Loja/Loja.jsx' 
+import Login from './Pages/Home/Logins/Login.jsx'
+import Loja from './Pages/Home/Loja/Loja.jsx'
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Solustech />} />
       <Route path="/Solustech" element={<Solustech />} />
       <Route path="/login" element={<Login />} />
-       <Route path="/Loja" element={<Loja />} /> 
+      <Route path="/Loja" element={<Loja />} />
     </Routes>
   )
 }
